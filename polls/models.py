f4291af7ce240130fc998e27af9ca061483cd225
dongwoo2 <ieiedong@naver.com>
2024-01-20 23:01:18 +0900

    [django] 장고 튜토리얼 part 2 (v4.2) 완강

--- polls/models.py
+++ polls/models.py
@@ -1,13 +1,25 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 
 class Question(models.Model): # 테이블
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.question_text
+        #return f'{self.id}.{self.question_text}'
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model): # 테이블
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # CASCADE는 무결성을 지키기위해 부모에 해당하는 참조값이 지워지면 자식에 해당되는 애들도 지워짐
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
