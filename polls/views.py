b27372219faf369f9485216479842409d26f4eb8
dongwoo2 <ieiedong@naver.com>
2024-01-21 22:27:35 +0900

    파이썬 장고 3번째 강의 40분

--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,33 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse # 응답
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    print('클라이언트의 요청을 받음')
-    input('요청을 처리하는 상태(완료하려면 엔터)')
-    return HttpResponse("Hello, world. You're at the polls index.") # 처리 된 객체를 response 객체로 만들어줌
-    #응답 할 내용이 body에 들어감
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template("polls/index.html") # polls에 있는 index.html 파일을 불러들인 것
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+#def index(request):
+    #print('클라이언트의 요청을 받음')
+   # input('요청을 처리하는 상태(완료하려면 엔터)')
+    #return HttpResponse("Hello, world. You're at the polls index.") # 처리 된 객체를 response 객체로 만들어줌
+    #응답 할 내용이 body에 들어감
