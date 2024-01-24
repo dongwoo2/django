63a7715e2d3e391baf4d652225c28fc985749e87
dongwoo2 <ieiedong@naver.com>
2024-01-24 10:27:44 +0900

    20240124 보라카이 둘째날 파이선공부

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+#from django.http import Http404
 
 # Create your views here.
 
 from django.http import HttpResponse # 응답
-from django.template import loader
+#from django.template import loader
 from .models import Question
 
 
@@ -16,12 +17,16 @@ def index(request):
     #return HttpResponse(template.render(context, request))
     return render(request, "polls/index.html", context)
 
+ #def detail(request, question_id):
+ #  try:
+ #       question = Question.objects.get(pk=question_id)
+ #   except Question.DoesNotExist:
+ #      raise Http404("Question does not exist")
+ #   return render(request, "polls/detail.html", {"question" : question})
+
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, "polls/detail.html", {"question" : question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question":question})
 
 
 def results(request, question_id):
